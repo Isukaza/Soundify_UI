a6bc2f634430a03b066bce296e7de78b48c0d865
Max Kakunia <maksimkharkov54@gmail.com>
2025-05-17 18:07:03 +0300

    Google authorization methods have been added to the manager

--- src/managers/AuthManager.ts
+++ src/managers/AuthManager.ts
@@ -1,8 +1,52 @@
 import {jwtDecode} from "jwt-decode";
 import {useStore} from "@/stores";
+import AuthApi from '@/api/AuthApi.js';
 
 export class AuthManager {
     private constructor() {
+    }
+
+    static async loginWithEmail(email: string, password: string): Promise<boolean> {
+        const {setJwt, setRefresh, setExp} = useStore.getState().auth;
+
+        const resp = await AuthApi.login(email, password);
+        if (resp.status) {
+            const {bearer, refreshToken} = resp.data;
+            const exp = jwtDecode<{ exp: number }>(bearer).exp;
+
+            setJwt(bearer);
+            setRefresh(refreshToken);
+            setExp(exp);
+
+            return true;
+        }
+        return false;
+    }
+
+    static async redirectToGoogleSSO(): Promise<void> {
+        const resp = await AuthApi.GetLoginGoogleSsoURL();
+        if (resp.status) {
+            window.location.href = resp.data;
+        }
+    }
+
+    static async handleGoogleCallback(code: string): Promise<boolean> {
+        const {setJwt, setRefresh, setExp} = useStore.getState().auth;
+
+        const resp = await AuthApi.HandleGoogleCallback(code);
+        if (resp.status) {
+            const {bearer, refreshToken} = resp.data;
+            const exp = jwtDecode<{ exp: number }>(bearer).exp;
+
+            setJwt(bearer);
+            setRefresh(refreshToken);
+            setExp(exp);
+
+            return true;
+        } else {
+            console.error('Google Auth Failed');
+            return false;
+        }
     }
 
     static refreshTokens(newJwt: string, newRefresh: string) {
@@ -15,6 +59,9 @@ export class AuthManager {
         }
 
         const state = useStore.getState();
+        state.auth.setEmail('');
+        state.auth.setPassword('');
+
         state.auth.setJwt(newJwt);
         state.auth.setRefresh(newRefresh);
         state.auth.setExp(newExp);
