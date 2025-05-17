17bccfc0a8e6c1c9e1b47cf3151a57a468760287
Max Kakunia <maksimkharkov54@gmail.com>
2025-05-17 18:06:22 +0300

    AuthSlice added authorization data

--- src/stores/slices/authSlice.ts
+++ src/stores/slices/authSlice.ts
@@ -1,20 +1,31 @@
 import {createSliceSetters, SliceCreator} from '@/stores/utils';
 
 export interface AuthSlice {
+    email: string;
+    password: string;
+
     jwt: string;
     refresh: string;
     exp: number;
 
+    setEmail: (email: string) => void;
+    setPassword: (password: string) => void;
     setJwt: (jwt: string) => void;
     setRefresh: (refresh: string) => void;
     setExp: (exp: number) => void;
 }
 
 const initialState: AuthSlice = {
+    email: '',
+    password: '',
     jwt: '',
     refresh: '',
     exp: 0,
 
+    setEmail: () => {
+    },
+    setPassword: () => {
+    },
     setJwt: () => {
     },
     setRefresh: () => {
