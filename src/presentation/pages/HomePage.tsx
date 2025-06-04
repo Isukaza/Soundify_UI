6c2b4d2691c07d73742c9525ed25a874779cce2e
Max Kakunia <maksimkharkov54@gmail.com>
2025-06-04 12:31:11 +0300

    The home page has been redesigned

--- src/presentation/pages/HomePage.tsx
+++ src/presentation/pages/HomePage.tsx
@@ -1,12 +1,7 @@
 import AbstractTrackManager from "@/domain/managers/Base/AbstractTrackManager";
+import {borderRadiusStyle} from "@/presentation/styles/common/borderRadiusStyle";
 import {useEffect} from "react";
-import {Link} from 'react-router-dom';
-import Button from '@mui/joy/Button';
-import {Stack} from '@mui/joy';
-import Typography from '@mui/joy/Typography';
-
-import Logo from '@/presentation/components/common/Logo';
-import {borderRadiusStyle} from '@/presentation/styles/common/borderRadiusStyle';
+import {Button, Stack} from '@mui/joy';
 
 import useInjectMap from "@/domain/hooks/useInjectMap";
 import AbstractAuthManager from '@/domain/managers/Base/AbstractAuthManager';
@@ -39,38 +34,13 @@ export default function HomePage() {
     }, [instances.trackManager]);
 
     return (
-        <Stack spacing={2} sx={{height: "auto", width: '1734px', alignItems: 'center'}}>
-            <Logo/>
-
-            <Typography level="h1" sx={{textAlign: 'center', fontWeight: 'bold'}}>
-                Thank you for visiting!
-            </Typography>
-            <Typography level="h1" sx={{textAlign: 'center', fontWeight: 'bold'}}>
-                {"We're in the early stages of developing something exciting, and we're glad you're here."}
-            </Typography>
-            <Typography level="h1" sx={{textAlign: 'center', fontWeight: 'bold'}}>
-                {'Our app is still under construction, but we\'re working hard to bring you a great experience.'}
-            </Typography>
-
-            <Button
-                component={Link}
-                to="/Login"
-                onClick={handleLogout}
-                variant="solid"
-                color="primary"
-                size="lg"
-                disabled={loading || !instances.authManager}
-                sx={{
-                    width: {xs: 'auto', sm: '25%'},
-                    ...borderRadiusStyle,
-                    '&:hover': {transform: 'scale(1.1)'},
-                }}
-            >
-                Log Out
-            </Button>
-
+        <Stack sx={{height: "auto", width: '50%', alignItems: 'center'}}>
+            <Stack direction="row" spacing={1} sx={{width: '100%', justifyContent: 'flex-start', mt: 1}}>
+                <Button sx={{...borderRadiusStyle}}>Track</Button>
+                <Button sx={{...borderRadiusStyle}}>Album</Button>
+                <Button sx={{...borderRadiusStyle}}>Artist</Button>
+            </Stack>
             <TrackTable/>
-
         </Stack>
     );
 }
